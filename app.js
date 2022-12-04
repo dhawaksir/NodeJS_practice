3c21f67932b5a77e0a23d552bc6e018e04d4c27a
suraj jung chhetri <dhawaksir@gmail.com>
2022-12-04 16:19:57 +0545

    Testing github

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+console.log('Testing Github')
+
 app.listen(3000, () => {
     console.log('The server is running in the port 3000');
 });
